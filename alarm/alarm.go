e2d20a26e031f46b8c5b39feb51c4147be203d9d
Romain Viau <romain.viau@gmail.com>
2019-01-12 22:41:49 +0100

    Succesfully parse a cronTab

--- alarm/alarm.go
+++ alarm/alarm.go
@@ -1,19 +1,46 @@
 package alarm
 
 import (
-	// "github.com/gorhill/cronexpr"
+
 	// "time"
 	"fmt"
 	"io/ioutil"
+	"regexp"
+	"strings"
+
+	"github.com/gorhill/cronexpr"
 )
 
+// Rule store cron parse result and other things
+type Rule struct {
+	CronExpression *cronexpr.Expression
+	Enable         bool
+	Special        bool
+}
+
 // Load Crontab
-func Load() {
+func Load() []Rule {
 	b, err := ioutil.ReadFile("crontab.txt")
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil
 	}
 	str := string(b)
-	fmt.Println(str)
+	rules := make([]Rule, 0)
+	regex := regexp.MustCompile("[0-9*/LW -]+(SUN|MON|TUE|WED|THU|FRI|SAT)?")
+	for idx, row := range strings.Split(str, "\n") {
+		fmt.Println("Row ", idx, row)
+		if cr := regex.FindString(row); cr != "" {
+			fmt.Println("Found: ", cr)
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Println("Following line malformed: ", idx)
+				}
+			}()
+			parsed := cronexpr.MustParse(cr)
+			rules = append(rules, Rule{parsed, strings.HasPrefix(cr, "#"), false})
+		}
+	}
+	return rules
+
 }
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	// "log"
+	// "net/http"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/romainv42/go-alarm-clock/alarm"
 )
 
 func main() {
-	alarm.Load()
+	for i, nxt := range alarm.Load() {
+		fmt.Print(i)
+		fmt.Println(nxt.CronExpression.Next(time.Now()))
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("./assets")))
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
