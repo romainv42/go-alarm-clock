8d3e223c4f06d17476f7c6bc853466bcb8361b15
Romain Viau <romain.viau@gmail.com>
2019-02-01 22:25:00 +0100

    Fix light level

--- assets/src/components/light.js
+++ assets/src/components/light.js
@@ -8,6 +8,6 @@ module.exports = {
     m("div", [
       m("i.fa-lightbulb", { class: `${Light.state.isOn ? "fas" : "far"}`, onclick: Light.switch })
     ]),
-    m("input", { type: "range", min: 0, max: 255, value: Light.state.level, oninput: Light.changeState, disabled: !Light.state.isOn })
+    m("input", { type: "range", min: 0, max: 255, value: Light.state.level, oninput: e => Light.changeState(e.target.value), disabled: !Light.state.isOn })
   ])
 };
